9d2890d77d35f54034e73a1d1cbce50954532368
Lam1ro4eQ <alsajgushev@gmail.com>
2023-02-14 17:51:55 +0300

    first commit

---
+++ lesson2/app.js
@@ -0,0 +1,26 @@
+const App = {
+    data() {
+        return {
+            placeholderString: 'Введите название заметки',
+            title: 'Список заметок',
+            inputValue: '',
+            notes: ['Заметка 1', 'Заметка 2']
+        }
+    },
+    methods: {
+        inputChangeHandler(event) {
+            this.inputValue = event.target.value
+        },
+        addNewNote() {
+            this.notes.push(this.inputValue)
+            this.inputValue = ''
+        },
+        deleteNote(index) {
+            this.notes.splice(index,1)
+        }
+    }
+}
+
+
+
+Vue.createApp(App).mount('#app')
